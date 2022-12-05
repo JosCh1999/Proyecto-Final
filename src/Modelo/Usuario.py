e6febaad74f29fb57b5d23c5e89021f81a81ee85
KevinCM27 <113396807+KevinCM27@users.noreply.github.com>
2022-12-05 00:55:29 -0500

    Update Usuario.py

--- src/Modelo/Usuario.py
+++ src/Modelo/Usuario.py
@@ -1,1 +1,9 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
 
+from declarative_base import Base
+
+class Usuario(Base):
+    __tablename__ = 'Usuario'
+
+    usuario = Column(String)
+    contraseña = Column(String)
