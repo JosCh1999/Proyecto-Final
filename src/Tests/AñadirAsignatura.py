640297ac910c46e6247646c7321d988eb86bdd54
KevinCM27 <74906627@continental.edu.pe>
2022-12-05 16:57:01 -0500

    Se añade test

---
+++ src/Tests/AñadirAsignatura.py
@@ -0,0 +1,11 @@
+import unittest
+from src.Modelo.Asignatura import Asignatura
+from src.Modelo.Usuario import Usuario
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
